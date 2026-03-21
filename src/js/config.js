447dc10dac28a88da056e2d6aea6b0fd7f04de4d
molamike <120581758+molamikedevs@users.noreply.github.com>
2026-03-21 17:06:59 +0400

    Fix Vercel deployment error

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const TIMEOUT_SEC = 2.5;
+export const RESULT_PER_PAGE = 6;
+export const GENRE_ANIMATION_ID = 16;
+
+export const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+export const TMDB_API_KEY = process.env.TMDB_API_KEY;
